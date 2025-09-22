87a7b0cde566c7d400436668db40853d3d789cf9
stagira <philippe@stagira.net>
2025-09-22 19:31:21 +0200

    create module

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,11 @@
 # data_manager.py
 
+class Tirage:
+    def __init__(self, date, numeros, etoiles):
+        self.date = date
+        self.numeros = numeros
+        self.etoiles = etoiles
+
 def charger_donnees():
     """Cette fonction sera responsable de charger l'historique des tirages."""
     print("Module data_manager : chargement des données...")
--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 import data_manager
 
-print("Démarrage de l'application principale.")
-historique_tirages = data_manager.charger_donnees()
-print("Application terminée.")
+# On crée un "objet" en utilisant le plan data_manager.Tirage
+premier_tirage = data_manager.Tirage(
+    date="20/10/2023", 
+    numeros=[5, 12, 22, 35, 48], 
+    etoiles=[3, 9]
+)
+
+# On accède aux données de façon super lisible !
+print(f"Date du tirage : {premier_tirage.date}")
+print(f"Numéros : {premier_tirage.numeros}")
+print(f"Étoiles : {premier_tirage.etoiles}")
