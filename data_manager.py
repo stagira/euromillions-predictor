9f1bd3b42045940b86d643d612d47a499b6b616a
stagira <philippe@stagira.net>
2025-09-22 19:32:51 +0200

    addeddocstrings

--- data_manager.py
+++ data_manager.py
@@ -1,13 +1,21 @@
 # data_manager.py
 
 class Tirage:
+    """Représente un seul tirage de l'Euromillions."""
     def __init__(self, date, numeros, etoiles):
+        """
+        Initialise un objet Tirage.
+
+        Args:
+            date (str): La date du tirage au format JJ/MM/AAAA.
+            numeros (list): Une liste de 5 entiers pour les numéros.
+            etoiles (list): Une liste de 2 entiers pour les étoiles.
+        """
         self.date = date
         self.numeros = numeros
         self.etoiles = etoiles
 
 def charger_donnees():
-    """Cette fonction sera responsable de charger l'historique des tirages."""
+    """Charge l'historique complet des tirages."""
     print("Module data_manager : chargement des données...")
-    # Pour l'instant, on retourne une liste vide.
     return []
